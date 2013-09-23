664c998d076af63d03ba0ec96aec63f062fa406e
shasha <eileenss8824@gmail.com>
2013-09-23 16:37:32 -0700

    first commit

---
+++ CodeKataTwo/CodeKataTwoTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace CodeKataTwo
+{
+    public class CodeKataTwoTest
+    {
+        [Fact]
+        public void shouldRunTheTest()
+        {
+            Assert.Equal(4,2+2);
+        }
+    }
+}
