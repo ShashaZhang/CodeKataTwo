a88ba4608ad75bb8f524320bbde1edb3ee9d6abd
shasha <eileenss8824@gmail.com>
2013-09-23 18:13:24 -0700

    [Shasha] Run through all the Array to find one element.

--- CodeKataTwo/CodeKataTwoTest.cs
+++ CodeKataTwo/CodeKataTwoTest.cs
@@ -5,9 +5,36 @@ namespace CodeKataTwo
     public class CodeKataTwoTest
     {
         [Fact]
-        public void shouldRunTheTest()
+        public void shouldReturnMinusOneWhenArrayIsEmpty()
         {
-            Assert.Equal(4,2+2);
+            SortTool sortTool = new SortTool();
+            int[] array = new int[]{};
+
+            int position = sortTool.chop(3, array);
+
+            Assert.Equal( -1 ,  position );
+        }
+
+        [Fact]
+        public void shouldReturnMinusOneWhenElementNotInArray()
+        {
+            SortTool sortTool = new SortTool();
+            int[] array = new int[]{1};
+
+            int position = sortTool.chop(3, array);
+
+            Assert.Equal(-1, position);
+        }
+
+        [Fact]
+        public void shouldReturnZeroWhenElmentInTheFirstPlaceOfTheArray()
+        {
+            SortTool sortTool = new SortTool();
+            int[] array = new int[]{1};
+
+            int position = sortTool.chop(1, array);
+
+            Assert.Equal(0, position);
         }
     }
 }
---
+++ CodeKataTwo/SortTool.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeKataTwo
+{
+    public class SortTool
+    {
+        public int chop(int goalValue, int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == goalValue)
+                    return i;
+            }
+                return -1;
+        }
+    }
+}
